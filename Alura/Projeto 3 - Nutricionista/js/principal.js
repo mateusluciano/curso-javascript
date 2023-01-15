d2e871701a64b37bf8d73a510eb5bdb28a2f8b21
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-01-14 22:51:11 -0300

    Modificando cores nos erros

--- Alura/Projeto 3 - Nutricionista/js/principal.js
+++ Alura/Projeto 3 - Nutricionista/js/principal.js
@@ -19,12 +19,18 @@ for(c=0 ; c < pacientes.length ; c++) {
     if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido!");
         pesoEhValido = false;
+        tdImc.style.backgroundColor='red';
+        tdImc.style.color='white';
+        tdImc.style.fontWeight='bold';
         tdImc.textContent = "Peso inválido";
     }
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
+        tdImc.style.backgroundColor='red';
+        tdImc.style.color='white';
+        tdImc.style.fontWeight='bold';
         tdImc.textContent = "Altura inválida";
     }
 
